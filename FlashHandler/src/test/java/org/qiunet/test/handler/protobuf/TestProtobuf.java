e0c253ab5feeaca9a0e0b610e1e1017ecbe70cad
qiunet <qiunet@gmail.com>
2022-11-07 15:09:30 +0800

    Revert "feat(FlashHandler):  ChannelMessage 打印放到序列化后面"
    This reverts commit 0a3ca57d7cd5fd04d61014ac3cc84d83e5904105.

--- FlashHandler/src/test/java/org/qiunet/test/handler/protobuf/TestProtobuf.java
+++ FlashHandler/src/test/java/org/qiunet/test/handler/protobuf/TestProtobuf.java
@@ -49,14 +49,6 @@ public class TestProtobuf {
 			Assertions.assertEquals(decode.getLevel(), level);
 			Assertions.assertEquals(decode.getList(), longs);
 		}
-	}
 
-	@Test
-	public void testEncode() {
-		List<Long> longs = Lists.newArrayList(1L, null, 3L);
-		String name = "qiunet";
-		int level = 10;
-		RequestData data = RequestData.valueOf(name, level, longs);
-		Assertions.assertThrows(NullPointerException.class, data::toByteBuf);
 	}
 }
